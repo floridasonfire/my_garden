af3a9c73b187c77be82c1538eab559ed30ab75a6
Jessica Engel & Franz Knupfer <student@epicodus.com>
2015-04-27 11:41:01 -0700

    add new spec that tells us if a plant is not in the hash

--- spec/my_garden_spec.rb
+++ spec/my_garden_spec.rb
@@ -15,19 +15,24 @@ describe(MyGarden) do
   end
 end
 
-  describe(MyGarden) do
-    describe("#my_fetch") do
-      it("retrieves a stored value by its key") do
-        test_hash = MyGarden.new()
-        test_hash.my_store("thyme", 1)
-        expect(test_hash.my_fetch("thyme")).to(eq(1))
-      end
-      it("retrieves multiple store values by their keys") do
-        test_hash = MyGarden.new()
-        test_hash.my_store("thyme", 1)
-        test_hash.my_store("rosemary", 4)
-        test_hash.my_store("oregano", 5)
-        expect(test_hash.my_fetch("rosemary")).to(eq(4))
-      end
+describe(MyGarden) do
+  describe("#my_fetch") do
+    it("retrieves a stored value by its key") do
+      test_hash = MyGarden.new()
+      test_hash.my_store("thyme", 1)
+      expect(test_hash.my_fetch("thyme")).to(eq(1))
+    end
+    it("retrieves multiple store values by their keys") do
+      test_hash = MyGarden.new()
+      test_hash.my_store("thyme", 1)
+      test_hash.my_store("rosemary", 4)
+      test_hash.my_store("oregano", 5)
+      expect(test_hash.my_fetch("rosemary")).to(eq(4))
+    end
+    it("returns that a plant isn't in your garden if it's not in the key") do
+      test_hash = MyGarden.new()
+      test_hash.my_store("thyme", 1)
+      expect(test_hash.my_fetch("sage")).to(eq("That's not in your garden!"))
     end
   end
+end
