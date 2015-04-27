4f323113a9850d5ece02da32e3ba32c74c0e7025
Jessica Engel & Franz Knupfer <student@epicodus.com>
2015-04-27 11:34:09 -0700

    Fetch values by their keys

--- lib/my_garden.rb
+++ lib/my_garden.rb
@@ -3,7 +3,6 @@ class MyGarden
     # new_hash = Hash.new()
     # new_hash.store(plant_type, number)
     @my_hash =Hash.new()
-
   end
 
   define_method(:my_store) do |plant_type, number|
@@ -13,8 +12,19 @@ class MyGarden
     @my_hash
   end
 
-  define_method(:my_fetch) do |plant_type|
-    @number
+  define_method(:my_fetch) do |plant_key|
+    plant_array = @my_hash.to_a()
+    plant_array.flatten!()
+
+    if plant_array.include?(plant_key)
+      plant_key_index=plant_array.index(plant_key)
+      number_index=plant_key_index+1
+      plant_array.at(number_index)
+      #=make stuff happen
+    else
+      "That's not in your garden!"
+    end
+    
   end
 
 end
--- spec/my_garden_spec.rb
+++ spec/my_garden_spec.rb
@@ -1,21 +1,33 @@
 require('rspec')
 require('my_garden')
 
-# describe(MyGarden) do
-#   describe("#my_fetch") do
-#     it("retrieves a stored value by its key") do
-#       test_hash = MyGarden.new("thyme", 1)
-#       expect(test_hash.my_fetch("thyme")).to(eq(1))
-#     end
-#
-#   end
-# end
-
 describe(MyGarden) do
   describe("#my_store") do
     it("stores keys and values into a hash") do
+      test_hash = MyGarden.new()
+      expect(test_hash.my_store("rosemary", 4)).to(eq({"rosemary"=>4}))
+    end
+    it("stores multiple keys and values into a hash") do
+      test_hash = MyGarden.new()
+      test_hash.my_store("rosemary", 4)
+      expect(test_hash.my_store("thyme", 6)).to(eq({"rosemary"=>4, "thyme"=>6}))
+    end
+  end
+end
+
+  describe(MyGarden) do
+    describe("#my_fetch") do
+      it("retrieves a stored value by its key") do
         test_hash = MyGarden.new()
-        expect(test_hash.my_store("rosemary", 4)).to(eq({"rosemary"=>4}))
+        test_hash.my_store("thyme", 1)
+        expect(test_hash.my_fetch("thyme")).to(eq(1))
+      end
+      it("retrieves multiple store values by their keys") do
+        test_hash = MyGarden.new()
+        test_hash.my_store("thyme", 1)
+        test_hash.my_store("rosemary", 4)
+        test_hash.my_store("oregano", 5)
+        expect(test_hash.my_fetch("rosemary")).to(eq(4))
       end
     end
   end
